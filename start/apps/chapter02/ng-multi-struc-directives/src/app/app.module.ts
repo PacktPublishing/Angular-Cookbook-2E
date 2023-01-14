eb51ab0446d5db94f5d76f7f4e4f08a679dbb327
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-01-14 21:53:46 +0100

    feat(multi-struc-dir): add starter files

---
+++ start/apps/chapter02/ng-multi-struc-directives/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
