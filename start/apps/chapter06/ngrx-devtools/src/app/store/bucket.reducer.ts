e01b3eb457fd6e0c5794d50e8924a3235fa08fea
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-03-05 22:53:53 +0100

    chroe(ngrx-devtools): starter files

---
+++ start/apps/chapter06/ngrx-devtools/src/app/store/bucket.reducer.ts
@@ -0,0 +1,17 @@
+import { createReducer, on } from '@ngrx/store';
+import { IFruit } from '../interfaces/fruit.interface';
+import { BucketActions } from './bucket.actions';
+
+export const initialState: ReadonlyArray<IFruit> = [];
+
+export const bucketReducer = createReducer(
+  initialState,
+  on(BucketActions.addFruit, (_state, { fruit }) => {
+    console.log({ fruit });
+    return [fruit, ..._state];
+  }),
+  on(BucketActions.removeFruit, (_state, { fruitId }) => {
+    console.log({ fruitId });
+    return _state.filter((fr) => fr.id === fruitId);
+  })
+);
