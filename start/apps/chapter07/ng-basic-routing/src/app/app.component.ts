63a887249fe43891c1d13a19638356accd3f5647
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-03-16 11:25:26 +0100

    chore(ng-basic-routes): starter files

---
+++ start/apps/chapter07/ng-basic-routing/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class AppComponent {}
---
+++ start/apps/chapter07/ng-basic-routing/src/main.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [],
+}).catch((err) => console.error(err));
