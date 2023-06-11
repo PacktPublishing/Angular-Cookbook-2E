965a590d53130c89dbc25825319cf27c3a4ed384
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-06-11 11:12:50 +0200

    feat(ng-cdk-virtual-scroll): recipe finalized

---
+++ final/apps/chapter09/ng-cdk-virtual-scroll/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { AppUserCard } from './interfaces/app-user-card.interface';
+import { DataService } from './core/data.service';
+import { LoaderComponent } from './components/loader/loader.component';
+import { TheAmazingListComponent } from './components/the-amazing-list/the-amazing-list.component';
+
+const LOADER_TIMEOUT = 3000;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, LoaderComponent, TheAmazingListComponent],
+})
+export class AppComponent {
+  $listItemsData!: Observable<AppUserCard[]>;
+  showLoader = false;
+  constructor(private dataService: DataService) {  }
+
+  getData() {
+    this.showLoader = true;
+    setTimeout(() => {
+      this.showLoader = false;
+      this.$listItemsData = this.dataService.getUsers();
+    }, LOADER_TIMEOUT)
+  }
+}
--- final/apps/chapter09/ng-cdk-virtual-scroll/src/app/components/the-amazing-list/the-amazing-list.component.ts
+++ final/apps/chapter09/ng-cdk-virtual-scroll/src/app/components/the-amazing-list/the-amazing-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { AppUserCard } from '../../interfaces/app-user-card.interface';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-the-amazing-list',
   templateUrl: './the-amazing-list.component.html',
   styleUrls: ['./the-amazing-list.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, ScrollingModule]
 })
 export class TheAmazingListComponent {
   @Input() listItems: AppUserCard[] = [];
