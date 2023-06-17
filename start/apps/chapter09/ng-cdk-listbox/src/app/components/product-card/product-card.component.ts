58561ec31a07c752f8cfeb48405e8858bcabbebe
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-06-17 21:37:29 +0200

    feat(ng-cdk-listbox): starter files

--- start/apps/chapter09/ng-cdk-listbox/src/app/components/product-card/product-card.component.ts
+++ start/apps/chapter09/ng-cdk-listbox/src/app/components/product-card/product-card.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  productForm = new FormGroup({
+    color: new FormControl('', [Validators.required])
+  })
+
+  constructor() {
+    this.productForm.valueChanges.subscribe(val => {
+      console.log('value changed', val);
+    })
+  }
+
+  formSubmit(form: FormGroup) {
+    console.log({
+      value: form.value
+    });
+  }
+}
