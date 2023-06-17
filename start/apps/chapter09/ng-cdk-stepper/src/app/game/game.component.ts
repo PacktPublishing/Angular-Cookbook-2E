4cbd8ba736323ac517c0e25d2e503905e4cac3a4
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-06-17 19:49:58 +0200

    feat(ng-cdk-stepper): starter files

---
+++ start/apps/chapter09/ng-cdk-stepper/src/app/game/game.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ValueGuesserComponent } from '../components/value-guesser/value-guesser.component';
+import { LeaderBoardComponent } from '../components/leader-board/leader-board.component';
+import { DiceComponent } from '../components/dice/dice.component';
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule, ValueGuesserComponent, LeaderBoardComponent, DiceComponent]
+})
+export class GameComponent {
+  nameForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+  });
+
+  submitName() {
+    // on game name submit
+  }
+}
