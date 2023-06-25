85a37144d7b8e1a9e4e7b2ec4e2737810ccaae96
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-06-25 15:55:38 +0200

    feat(ng-jest-setup): final files

---
+++ final/apps/chapter10/ng-jest-setup/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, CounterComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
