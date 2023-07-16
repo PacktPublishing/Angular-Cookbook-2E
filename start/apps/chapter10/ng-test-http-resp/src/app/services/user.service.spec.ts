ada6553e7f2ad4b5db762ac56103297a3b396b72
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-07-17 01:18:33 +0200

    feat(ng-test-http-resp): WIP

--- start/apps/chapter10/ng-test-http-resp/src/app/services/user.service.spec.ts
+++ start/apps/chapter10/ng-test-http-resp/src/app/services/user.service.spec.ts
@@ -2,18 +2,51 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, HttpClientTestingModule],
     });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected user data (HttpClient called once)', (done) => {
+    const expectedData = [
+      { id: 1, name: 'User A' },
+      { id: 2, name: 'User B' },
+    ];
+
+    service.getUsers().subscribe({
+      next: (data) => {
+        expect(data).toEqual(expectedData);
+        done();
+      },
+      error: (err) => {
+        console.log('Error: ', err);
+      },
+    });
+
+    const req = httpTestingController.expectOne('assets/fake-users.json');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expectedData); // Respond with mocked data
   });
 });
