41aba1dd2059e36c7dcb942e68cc4047d4b59fb8
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-07-20 00:50:35 +0200

    feat(ng-cypress-starter): starter files

---
+++ start/apps/chapter11/ng-cypress-starter/src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent, RouterTestingModule],
+    }).compileComponents();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.toolbar__title')?.textContent).toContain(
+      'Your first Cypress test in Angular'
+    );
+  });
+});
