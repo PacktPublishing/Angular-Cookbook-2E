1f4f486c6119b913d46966beba10848448bb6e44
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-04 00:06:43 +0200

    feat(ng-cy-element-visibility): final files

---
+++ final/apps/chapter11/ng-cy-element-visibility-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,25 @@
+import { getCounterActions, getCounterCard, getHeaderTitle, getToggleCounterButton } from '../support/app.po';
+
+describe('ng-cypress-starter', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display the correct header title', () => {
+    getHeaderTitle().should('contain.text', 'Validating if a DOM element is visible on the view');
+  });
+
+  it('should toggle visibility of counter card when the toggle button is clicked', () => {
+    getCounterCard().should('exist');
+    getToggleCounterButton().click();
+    getCounterCard().should('not.exist');
+    getToggleCounterButton().click();
+    getCounterCard().should('exist');
+  });
+
+  it('should show the action buttons when the counter card is hovered', () => {
+    getCounterCard().realHover();
+    getCounterActions().should('have.length', 3);
+    getCounterActions().contains('Increment').should('be.visible');
+    getCounterActions().contains('Decrement').should('be.visible');
+    getCounterActions().contains('Reset').should('be.visible');
+  })
+});
