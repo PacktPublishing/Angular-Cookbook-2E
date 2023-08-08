6f8eb90952d82334cd7be055266a46de53c76598
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-08 16:09:09 +0200

    feat(ng-on-push-strat): starter files

--- final/apps/chapter12/ng-on-push-strategy/src/app/app.config.ts
+++ final/apps/chapter12/ng-on-push-strategy/src/app/app.config.ts
@@ -5,14 +5,9 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { RANDOMIZATION_COUNT } from './tokens';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
-    {
-      provide: RANDOMIZATION_COUNT,
-      useValue: 9
-    }
   ],
 };
--- final/apps/chapter12/ng-on-push-strategy/src/app/component/user-card/user-card.component.ts
+++ final/apps/chapter12/ng-on-push-strategy/src/app/component/user-card/user-card.component.ts
@@ -1,28 +1,34 @@
-import { Component, Input, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IUser } from '../../interfaces/user.interface';
 import { RouterModule } from '@angular/router';
+import { LogsService } from '../../services/logs.service';
 import { randColor } from '@ngneat/falso';
-import { RANDOMIZATION_COUNT } from '../../tokens';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardComponent {
   @Input() user!: IUser;
   @Input() index = 0;
-  randomizationCount = inject(RANDOMIZATION_COUNT);
+  logsService = inject(LogsService);
 
   get randomColor() {
+    this.logsService.updateLogEntry(this.user.email);
     let color;
-    for (let i = 0; i < factorial(this.randomizationCount); i++) {
+    for (let i = 0; i < factorial(10); i++) {
       color = randColor();
     }
     return color;
+  }
+
+  get log() {
+    return this.logsService.logs[this.user.email] ?? 0;
   }
 }
 
