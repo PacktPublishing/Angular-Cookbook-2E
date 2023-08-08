3d8f287931312e4b755b2ed93fe15de78e0ac457
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-08 16:40:57 +0200

    feat(ng-on-push-strategy): final files

--- final/apps/chapter12/ng-on-push-strategy/src/app/app.config.ts
+++ final/apps/chapter12/ng-on-push-strategy/src/app/app.config.ts
@@ -5,9 +5,14 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { RANDOMIZATION_COUNT } from './tokens';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
+    {
+      provide: RANDOMIZATION_COUNT,
+      useValue: 9
+    }
   ],
 };
