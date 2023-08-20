91cbb3ed2658d839ace9af128d15fc60e1a059d4
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-21 00:28:09 +0200

    feat(ng-ww-perf): starter files

---
+++ start/apps/chapter12/ng-ww-perf/src/app/services/logs.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogsService {
+  logs: Record<string, number> = {}
+  updateLogEntry(email: string) {
+    if (this.logs[email] === undefined) {
+      this.logs = {
+        ...this.logs,
+        [email]: 1
+      }
+    } else {
+      this.logs = {
+        ...this.logs,
+        [email]: this.logs[email] + 1
+      }
+    }
+  }
+}
