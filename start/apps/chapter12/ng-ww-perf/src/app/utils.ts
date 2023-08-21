4e43eaaa0b96714dbd0b5e889383d2182dc1eb59
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-21 23:35:28 +0200

    feat(ng-ww-perf): starter files

---
+++ start/apps/chapter12/ng-ww-perf/src/app/utils.ts
@@ -0,0 +1,18 @@
+import { randColor } from "@ngneat/falso";
+
+export const factorial = (n: number): number => {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+export const generateRandomColor = (randomizationCount: number) => {
+  let color;
+  for (let i = 0; i < factorial(randomizationCount); i++) {
+    color = randColor();
+  }
+  return color;
+}
+
