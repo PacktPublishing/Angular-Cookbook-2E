2e229469dfc958ec23d5c92c7437cd6d568d4ec6
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-21 03:20:52 +0200

    WIP(ng-ww-perf): starter files

--- start/apps/chapter12/ng-ww-perf/src/app/component/user-card/user-card.component.ts
+++ start/apps/chapter12/ng-ww-perf/src/app/component/user-card/user-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IUser } from '../../interfaces/user.interface';
 import { RouterModule } from '@angular/router';
 import { randColor } from '@ngneat/falso';
 import { RANDOMIZATION_COUNT } from '../../tokens';
 import { LogsService } from '../../services/logs.service';
+import { getRandomColorWorker } from '../../workers';
 
 @Component({
   selector: 'app-user-card',
@@ -13,11 +14,19 @@ import { LogsService } from '../../services/logs.service';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
   @Input() user!: IUser;
   @Input() index = 0;
   randomizationCount = inject(RANDOMIZATION_COUNT);
   logsService = inject(LogsService);
+  worker: Worker = getRandomColorWorker();
+
+  ngOnInit() {
+    this.worker.onmessage = ({ data: { randomColor, email } }) => {
+      console.log('received message from worker', randomColor, email);
+    };
+    this.worker.postMessage({ randomizationCount: this.randomizationCount, email: this.user.email });
+  }
 
   get log() {
     return this.logsService.logs[this.user.email] ?? 0;
