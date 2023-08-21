fd04e6a92bb0a8bb0fa3e011d52df8bdf1e0e3d2
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-21 23:35:28 +0200

    feat(ng-ww-perf): starter files

--- start/apps/chapter12/ng-ww-perf/src/app/component/user-card/user-card.component.ts
+++ start/apps/chapter12/ng-ww-perf/src/app/component/user-card/user-card.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IUser } from '../../interfaces/user.interface';
 import { RouterModule } from '@angular/router';
-import { randColor } from '@ngneat/falso';
-import { RANDOMIZATION_COUNT } from '../../tokens';
 import { LogsService } from '../../services/logs.service';
-import { getRandomColorWorker } from '../../workers';
+import { generateRandomColor } from '../../utils';
+import { RANDOMIZATION_COUNT } from '../../tokens';
 
 @Component({
   selector: 'app-user-card',
@@ -14,19 +13,11 @@ import { getRandomColorWorker } from '../../workers';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent {
   @Input() user!: IUser;
   @Input() index = 0;
-  randomizationCount = inject(RANDOMIZATION_COUNT);
   logsService = inject(LogsService);
-  worker: Worker = getRandomColorWorker();
-
-  ngOnInit() {
-    this.worker.onmessage = ({ data: { randomColor, email } }) => {
-      console.log('received message from worker', randomColor, email);
-    };
-    this.worker.postMessage({ randomizationCount: this.randomizationCount, email: this.user.email });
-  }
+  randomizationCount = inject(RANDOMIZATION_COUNT);
 
   get log() {
     return this.logsService.logs[this.user.email] ?? 0;
@@ -34,19 +25,7 @@ export class UserCardComponent implements OnInit {
 
   get randomColor() {
     this.logsService.updateLogEntry(this.user.email);
-    let color;
-    for (let i = 0; i < factorial(this.randomizationCount); i++) {
-      color = randColor();
-    }
-    return color;
+    return generateRandomColor(this.randomizationCount);
   }
 }
 
-
-function factorial(n: number): number {
-  if (n == 0 || n == 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
