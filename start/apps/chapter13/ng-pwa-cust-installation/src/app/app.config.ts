7587d407b86bf9462ec2041d920c2184c85264c3
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-28 23:23:56 +0200

    feat(ng-pwa-cust-installation): starter files

---
+++ start/apps/chapter13/ng-pwa-cust-installation/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withHashLocation,
+} from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation(), withHashLocation()), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
+};
