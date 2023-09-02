21e4640427f81e8b08c4fad19023d05f441f19bd
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-02 10:30:56 +0200

    feat(ng-pwa-app-shell): final files

---
+++ final/apps/chapter13/ng-pwa-app-shell/src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from '../users/users.component';
+
+@Component({
+  selector: 'app-app-shell',
+  standalone: true,
+  imports: [CommonModule, UsersComponent],
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.css'],
+})
+export class AppShellComponent {}
