6bc55e56b1ac57b7e395e384f4088b10e8c5926a
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-20 21:40:23 +0200

    fix(ng-animating-lists): update to v16

--- final/apps/chapter04/ng-animating-lists/src/app/app.config.ts
+++ final/apps/chapter04/ng-animating-lists/src/app/app.config.ts
@@ -3,11 +3,11 @@ import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    provideAnimations(),
+    provideAnimations()
   ],
 };
