e011dace2f665eebd892c27572c3457c9ab83bac
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-09-21 20:56:18 +0200

    fix(ng-disable-animations): change app config to employee config

--- final/apps/chapter04/ng-disable-animations/jest.config.ts
+++ final/apps/chapter04/ng-disable-animations/jest.config.ts
@@ -3,16 +3,15 @@ export default {
   displayName: 'ng-disable-animations',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {},
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
   coverageDirectory: '../../../coverage/apps/chapter04/ng-disable-animations',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
