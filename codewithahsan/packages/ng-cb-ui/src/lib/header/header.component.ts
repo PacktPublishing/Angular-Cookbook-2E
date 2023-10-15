d648a5bb5e166505687d647ff0b09e1a82cb3904
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-10-15 17:07:53 +0200

    feat(lib): added a library for header

---
+++ codewithahsan/packages/ng-cb-ui/src/lib/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-cb-ui-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  @Input() appName = 'ng-cookbook-recipe';
+  @Input() appTitle = 'Angular Cookbook Recipe';
+}
--- start/apps/chapter14/ng-signals-intro/src/app/app.component.ts
+++ start/apps/chapter14/ng-signals-intro/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '@codewithahsan/ng-cb-ui';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeaderComponent],
 })
 export class AppComponent {
   tasks = [
