35e9d4c184ebb415973d18635347833c23c8820b
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-11-02 22:19:38 +0100

    WIP: ng-cdk-lkm starter files

--- start/apps/chapter09/ng-cdk-lkm/src/app/components/users-list-item/users-list-item.component.ts
+++ start/apps/chapter09/ng-cdk-lkm/src/app/components/users-list-item/users-list-item.component.ts
@@ -1,15 +1,16 @@
+
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppUserCard } from '../../interfaces/app-user-card.interface';
 
 @Component({
-  selector: 'app-the-amazing-list-item',
+  selector: 'app-users-list-item',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './the-amazing-list-item.component.html',
-  styleUrls: ['./the-amazing-list-item.component.scss'],
+  templateUrl: './users-list-item.component.html',
+  styleUrls: ['./users-list-item.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class TheAmazingListItemComponent {
+export class UsersListItemComponent {
   @Input() item!: Partial<AppUserCard>;
 }
