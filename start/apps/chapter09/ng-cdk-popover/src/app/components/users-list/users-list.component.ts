d91ae344762e5fd7309d78851ec043da3f572191
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-11-02 16:34:17 +0100

    fix(ng-cdk-popowver): WIP - start state

---
+++ start/apps/chapter09/ng-cdk-popover/src/app/components/users-list/users-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppUserCard } from '../../interfaces/app-user-card.interface';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { UsersListItemComponent } from '../users-list-item/users-list-item.component';
+
+@Component({
+  selector: 'app-users-list',
+  standalone: true,
+  imports: [CommonModule, ScrollingModule, UsersListItemComponent],
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss'],
+})
+export class UsersListComponent {
+  @Input() listItems: AppUserCard[] = [];
+}
+
