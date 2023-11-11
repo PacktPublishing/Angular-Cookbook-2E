d0129a99ef1af213e10e3b754e9c2b99f0e43b4b
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-11-11 23:57:15 +0100

    fix(ng-run-outside-angular): fix per AR

---
+++ final/apps/chapter12/ng-run-outside-angular/src/app/components/watch-time/watch-time.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+
+declare let window: any;
+
+@Component({
+  selector: 'app-watch-time',
+  templateUrl: './watch-time.component.html',
+  styleUrls: ['./watch-time.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WatchTimeComponent implements OnInit {
+  @Input() value!: number;
+  @Input() name!: string;
+  @Input() digits = 2;
+
+  ngOnInit(): void {
+    if (!window['appLogs']) {
+      window['appLogs'] = {};
+    }
+    window['appLogs'][this.name] = 0;
+  }
+
+  get timeVal() {
+    window['appLogs'][this.name]++;
+    let str = this.value.toString();
+    if (str.length === this.digits) {
+      return str;
+    }
+    for (let i = 0, len = this.digits - str.length; i < len; ++i) {
+      str = `0${str}`;
+    }
+    return str;
+  }
+}
