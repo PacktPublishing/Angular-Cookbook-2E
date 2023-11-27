696449ba11b2974f7adcbf8cfcf8d04f610b5b87
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-11-27 21:55:38 +0100

    feat(logo): add the new logo
    added the ng-logo component in the library
    not using it at the moment

---
+++ codewithahsan/packages/ng-cb-ui/src/lib/ng-logo/ng-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-cb-ui-ng-logo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ng-logo.component.html',
+  styleUrls: ['./ng-logo.component.scss'],
+})
+export class NgLogoComponent {}
