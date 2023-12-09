83a1facd56747264bc2fb23b3513f7c7509521dd
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-12-09 22:02:31 +0100

    chore(ng-perf-defer): final files

--- final/apps/chapter12/ng-perf-deferred/src/app/app.routes.ts
+++ final/apps/chapter12/ng-perf-deferred/src/app/app.routes.ts
@@ -4,9 +4,5 @@ export const appRoutes: Route[] = [
   {
     path: '',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
-  },
-  {
-    path: 'user/:uuid',
-    loadComponent: () => import('./user/user.component').then(m => m.UserComponent)
-  },
+  }
 ];
