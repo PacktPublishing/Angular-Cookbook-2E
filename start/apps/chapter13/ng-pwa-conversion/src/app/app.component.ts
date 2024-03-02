7494dad7f4497e4e5eb238b578c1a0e4406241cb
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-03-02 23:29:39 +0100

    refactor(ng-pwa-conversion): update header in the app from the comp library

--- start/apps/chapter13/ng-pwa-conversion/src/app/app.component.ts
+++ start/apps/chapter13/ng-pwa-conversion/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HeaderComponent } from '@codewithahsan/ng-cb-ui';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [HeaderComponent, CommonModule, RouterModule],
 })
 export class AppComponent {}
