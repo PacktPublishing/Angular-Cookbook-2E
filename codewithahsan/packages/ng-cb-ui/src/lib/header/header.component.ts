2ee140888e393d290fd515aa21b3c9b565126606
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-03-22 22:34:39 +0100

    feat(ng-cb-ui): add review widget side bar
    added review widget sidebar in the header

--- codewithahsan/packages/ng-cb-ui/src/lib/header/header.component.ts
+++ codewithahsan/packages/ng-cb-ui/src/lib/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgLogoComponent } from '../ng-logo/ng-logo.component';
+import { PULSE_CHECKER_SESSIONS } from '../../assets/pulsechecker-sessions';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'ng-cb-ui-header',
@@ -9,9 +11,23 @@ import { NgLogoComponent } from '../ng-logo/ng-logo.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Input() appName = 'ng-cookbook-recipe';
   @Input() appTitle = 'Angular Cookbook Recipe';
-}
+  reviewsShown = false;
+  sanitizer = inject(DomSanitizer);
 
-// TODO: remove the headers from all the projects via schematics
+  reviewLink!: SafeResourceUrl;
+
+  ngOnInit(): void {
+    this.reviewLink = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://pulsechecker.netlify.app/review-widget?sessionPin=${
+        PULSE_CHECKER_SESSIONS[this.appName].pin
+      }`
+    );
+  }
+
+  toggleReviewsShown() {
+    this.reviewsShown = !this.reviewsShown;
+  }
+}
