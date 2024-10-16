a88a1f81bc88545f9415edebe95029637eb1b1d3
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-10-16 07:36:55 +0200

    feat(signal-store): add signal store

--- start/apps/chapter01/ng-cc-signals/src/app/app.component.ts
+++ start/apps/chapter01/ng-cc-signals/src/app/app.component.ts
@@ -1,17 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal, viewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '@codewithahsan/ng-cb-ui';
+import { TodoStore } from './store/todos.store';
+import { FormsModule } from '@angular/forms';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [HeaderComponent, CommonModule, RouterModule, HeaderComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    HeaderComponent,
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    SnackbarComponent,
+  ],
 })
 export class AppComponent {
-  tasks = [
-    { title: 'Buy milk', completed: false },
-    { title: 'Read a book', completed: true },
-  ];
+  newTodoTitle = signal('');
+  store = inject(TodoStore);
+  snackbar = viewChild.required(SnackbarComponent);
+
+  constructor() {
+    effect(() => {
+      const allCompleted = this.store
+        .todos()
+        .every((todoItem) => todoItem.completed);
+      if (allCompleted) {
+        this.snackbar().show();
+      }
+    });
+  }
+
+  submitNewTodo() {
+    this.store.addTodo(this.newTodoTitle());
+    this.newTodoTitle.set('');
+  }
 }
